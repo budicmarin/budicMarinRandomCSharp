10db0ac7dde8c53394331b4f6b49f2368f46176c
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-05-19 12:59:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinRandomCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomGenerator rand=new RandomGenerator();
+            int i = rand.RandomNumber(1,1000);
+            Console.WriteLine(i);
+            string password = rand.RandomPassword();
+            Console.WriteLine(password);
+
+            string str= rand.RandomString(15, false);
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
